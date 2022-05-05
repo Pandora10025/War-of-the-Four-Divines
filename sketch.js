206e50f55bc80086a1be5b2a66e493712e50321b
Pandora10025 <98975655+Pandora10025@users.noreply.github.com>
2022-05-04 21:30:34 -0400

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,171 @@
+let introBot = new RiveScript();
+let characterBot = new RiveScript();
+let storyBot = new RiveScript();
+let bg
+let response = []
+let nextButton
+let pages = 0
+let indexResponses = 0
+
+function preload() {
+  introBot.loadFile("intro.txt").then(botLoaded).catch(error);
+  characterBot.loadFile("character.txt").then(botLoaded).catch(error);
+  storyBot.loadFile("story.txt").then(botLoaded).catch(error);
+
+}
+
+function botLoaded() {
+  console.log("Game loaded!");
+  introBot.sortReplies();
+  characterBot.sortReplies();
+  storyBot.sortReplies();
+}
+
+function error(_error) {
+  console.log(_error);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background("Green");
+
+  response.push("Type 'Start' to begin game.");
+
+  nextButton = createButton("Next");
+  nextButton.mousePressed(changePage);
+  nextButton.position()
+
+
+  inputField = createInput();
+  inputField.position(width * 2 / 6, height * 3 / 4)
+  inputField.hide()
+
+  submitBttn = createButton("Send")
+  submitBttn.mousePressed(chat);
+  submitBttn.position(width * 3 / 5, height * 3 / 4)
+  submitBttn.hide()
+
+}
+
+function mainGame() {
+  background("Green");
+
+  // textAlign(CENTER)
+  // text("This is the main page", width / 2, height / 2)
+}
+
+function introPage() {
+  background("Pink")
+  // textAlign(CENTER)
+  // text("This is the intro page", width / 2, height / 2)
+
+  inputField.show()
+  submitBttn.show()
+
+
+  displayResponses()
+  // playerInput()
+}
+
+function characterPage() {
+  background("Red")
+  // textAlign(CENTER)
+  // text("This is the character page", width / 2, height / 2)
+
+  // playerInput()
+  displayResponses()
+
+}
+
+function storyPage() {
+  background("blue")
+  // textAlign(CENTER)
+  // text("This is the story page", width / 2, height / 2)
+
+  // playerInput()
+  displayResponses()
+}
+
+function respond(responses) {
+  response.push(responses);
+  // console.log(repsonses);
+
+  
+}
+
+function displayResponses(){
+  textAlign(LEFT)
+ textSize(18);
+ rectMode(CENTER)
+// for (let i = 0; i < response.length; i++) {
+  text(
+    response[indexResponses],
+    width / 2 + 50 ,
+    height / 3 +30,
+    width/2,
+    height/3)
+  // if (width/ 3 + i * 60 + 50 > width) {
+  //   response.splice(0, 1);
+
+  // }
+// }
+}
+
+function changePage() {
+
+  if (pages == 3) {
+    pages = 0
+  }
+  pages++
+}
+
+// function playerInput(){
+//   inputField = createInput();
+//   inputField.position(width*2/6,height*3/4)
+
+//   submitBttn = createButton("Send")
+//   submitBttn.mousePressed(chat);
+//   submitBttn.position(width*3/5,height*3/4)
+
+//   keyPressed()
+// }
+
+function keyPressed() {
+  if (keyCode == ENTER) {
+    chat()
+  }
+}
+
+function chat() {
+
+  if (inputField.value()) {
+    let input = inputField.value();
+    if (pages == 1) {
+      introBot.reply("local-user", input).then(respond);
+    }
+    else if (pages == 2) {
+      characterBot.reply("locel-user", input).then(respond);
+    }
+    else if (pages == 3) {
+      storyBot.reply("local-user", input).then(respond);
+
+    }
+    inputField.value("")
+    indexResponses++
+  }
+}
+
+function draw() {
+  switch (pages) {
+    case 0: mainGame()
+      break;
+    case 1: introPage()
+      break;
+    case 2: characterPage()
+      break;
+    case 3: storyPage()
+      break;
+
+  }
+
+}
